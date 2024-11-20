f3e176aa53adb5d01b086cf0cf0ead6ea37c3607
Kyle McParland <kylemcparland@gmail.com>
2024-11-20 13:27:21 -0500

    Implement Admin Dashboard view to display values from DB

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = Product.count
+    @category_count = Category.count
+    @total_quantity = Product.sum(:quantity)
   end
 end
